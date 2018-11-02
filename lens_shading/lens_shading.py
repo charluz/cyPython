86c2ac49bd402b3233e128ba641a36288c0226c3
charlesCY <wuchangyu@gmail.com>
2018-11-02 14:25:20 +0800

    Add OpenRAW --> LoadRAW feature

--- lens_shading/lens_shading.py
+++ lens_shading/lens_shading.py
@@ -14,27 +14,51 @@ winMain = Tk()
 winMain.title('Lens Shading Viewer')
 #winMain.geometry('300x150')
 
+def messageBoxOK(title, msg):
+    box = Toplevel()
+    box.title(title)
+    Label(box, text=msg).pack()
+    Button(box, text='OK', command=box.destroy).pack()
+
+def cbfnButtonLoadRaw():
+    global btnRaw, rawdata
+    print("Button: Load RAW")
+    print(rawdata)
 
 def cbfnButtonOpenRaw():
-    global txtlblRawFName, btnOpenRaw
+    global txtlblRawFName, btnRaw, rawdata
     txtlblRawFName.set(value=filedialog.askopenfilename() )
-    print(txtlblRawFName.get())
+    rawfname = txtlblRawFName.get()
+    #print(rawfname)
+    try:
+        f = open(rawfname, 'rb')
+        rawdata = f.read()
+        btnRaw.config(text='Load RAW', command=cbfnButtonLoadRaw)
+    except:
+        messageBoxOK('FileIO', 'Failed to open file :\n' + rawfname)
+    '''
+    with open(rawfname, 'rb') as in_file:
+        rawdata = in_file.read()
+        btnRaw.config(text='Load RAW', command=cbfnButtonLoadRaw)
+    '''
 
 
-btnOpenRaw = Button(winMain, text='Open RAW', command=cbfnButtonOpenRaw)
-btnOpenRaw.grid(row=0, column=0, pady=2)
+btnRaw = Button(winMain, text='Open RAW', command=cbfnButtonOpenRaw)
+btnRaw.grid(row=0, column=0, pady=2)
 txtlblRawFName = StringVar()
 ##txtlblRawFName.set(value='test')
 lblRawFName = Label(winMain, width=48, textvariable=txtlblRawFName)
 lblRawFName.grid(row=0, column=1, columnspan=8)
 
 
-lblRawWidth = Label(winMain, text='Width').grid(row=1, column=0, pady=2)
+lblRawWidth = Label(winMain, text='Width')
+lblRawWidth.grid(row=1, column=0, pady=2)
 txtlblRawWidth = StringVar()
 entryRawWidth = Entry(winMain, bd=2, justify=LEFT, width=10, textvariable=txtlblRawWidth)
 entryRawWidth.grid(row=1, column=1, sticky=W)
 
-lblRawHeight = Label(winMain, text='Height').grid(row=2, column=0, pady=2)
+lblRawHeight = Label(winMain, text='Height')
+lblRawHeight.grid(row=2, column=0, pady=2)
 txtlblRawHeight = StringVar()
 entryRawHeight = Entry(winMain, bd=2, justify=LEFT, width=10, textvariable=txtlblRawHeight)
 entryRawHeight.grid(row=2, column=1, sticky=W)
@@ -44,7 +68,8 @@ def ShowChoice():
     global bayerSelect
     print(bayerSelect.get())
 
-lblRawBayer = Label(winMain, text='Bayer').grid(row=1, column=3, pady=2)
+lblRawBayer = Label(winMain, text='Bayer')
+lblRawBayer.grid(row=1, column=3, pady=2)
 bayerSelect = IntVar(value=3)
 bayer_config = [ ('R', 0, 2, 3), ('Gr', 1, 2, 4), ('Gb', 2, 3, 3), ('B', 3, 3, 4) ]
 #bayer_config = [ ('R', 0), ('Gr', 1), ('Gb', 2), ('B', 3) ]
@@ -58,7 +83,8 @@ for bayer, val, row, col in bayer_config:
     btn.config(anchor=W, justify=LEFT, width=2)
     print("Bayer= %s, Row= %d, Column= %d" % (bayer, row, col) )
 
-lblRawBits = Label(winMain, text='Pixel Bits').grid(row=3, column=0, padx=2, pady=2)
+lblRawBits = Label(winMain, text='Pixel Bits')
+lblRawBits.grid(row=3, column=0, padx=2, pady=2)
 txtlblRawBits = StringVar()
 entryRawBits = Entry(winMain, bd=2, justify=LEFT, width=10, textvariable=txtlblRawBits)
 entryRawBits.grid(row=3, column=1, sticky=W)
