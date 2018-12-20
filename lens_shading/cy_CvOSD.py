dbd6f5040a9db8e18e999b19750f745232df2a06
charlesCY <wuchangyu@gmail.com>
2018-12-20 16:41:29 +0800

    complete function and demo of osdText class

--- lens_shading/cy_CvOSD.py
+++ lens_shading/cy_CvOSD.py
@@ -7,17 +7,30 @@ import cv2
 # Class: CvOSD
 #--------------------------------------
 class osdText():
+    """Define a class to simplify putting osd text on an image with cv2.
+
+    Usage:
+    -------------
+    1. Import cy_CvOSD as OSD
+    1. osd = osdText()
+    1. osd.show(cv_img, text, X, Y)
+        * where X, Y is the coordinate of top-left point of the text
+    * Use set_property() to modify font, such as font face, color, scale, and thickness
+    * Use get_textSize() to calculate the width, height, margine of the string
+    * To calculate the coordinate of the "next line" :
+        1. call get_textSize("this string") to get width, height, pads
+        1. the starting (Xn,Yn) of "next line":
+            Xn= Xc, Yn= Yc+(h+pads*2)
+        1. the starting (Xn, Yn) of "next string":
+            Xn= Xc+(w+pads*2), Yn= Yc
     """
-    """
-    def __init__(self, text='', fface=cv2.FONT_HERSHEY_SIMPLEX, fcolor=(255, 255, 255), fscale=1, fthick=2):
+    def __init__(self, fface=cv2.FONT_HERSHEY_SIMPLEX, fcolor=(255, 255, 255), fscale=1, fthick=2):
         """
         """
         self.fontFace       = fface
         self.fontScale      = fscale
         self.fontColor      = fcolor
         self.fontThickness  = fthick
-        self.text           = text
-        self._update_dirty  = True
 
     def set_property(self, **kwargs):
         """
@@ -33,58 +46,44 @@ class osdText():
                 self.fontThickness = val
             else:
                 pass
-        self._update_dirty = True
 
-    def set_text(self, text):
-        self.text = text
-        self._update_dirty = True
-
-    def update(self, force_update=False):
+    def get_textSize(self, text):
+        """To calcualte the text(string) width, height, and margin
         """
-        """
-        if self._update_dirty == False and force_update == False:
-            return
-
         #-- get size of text
-        ((tw, th),tpad) = cv2.getTextSize(  text=self.text,
+        ((tw, th),tpad) = cv2.getTextSize(  text,
                                             fontFace=self.fontFace,
                                             fontScale=self.fontScale,
                                             thickness=self.fontThickness)
+        return tw, th, tpad
 
-             if False:
-                #print(nameID, ": shape= ", subGray.shape, shading rect 的 an= ", Ymean, " Gmean= ", Gmean)
-                Ytext = "Y= "+str(Ymean)
-                RGtext = "R/G= "+ "{:.2f}%".format(Rratio)
-                BGtext = "B/G= "+ "{:.2f}%".format(Bratio)
-                print(Ytext, ' ', RGtext, ' ', BGtext)
-
-                text = Ytext
-                fface = cv2.FONT_HERSHEY_SIMPLEX
-                fscale=1
-                fthick=2
-                ((tw, th),tpad) = cv2.getTextSize(text=Ytext, fontFace=fface, fontScale=fscale, thickness=fthick)
-                cv2.putText(cv_img, text, (VPt[0], VPb[1]+(th+tpad*2)), fface, fscale, (0, 0, 200), fthick, 255)
-                #text = nameID + ": Y= " + str(Ymean) + " (R,G,B)= " + str(Rmean) + ", " + str(Gmean) + ", " + str(Bmean)
-                #print(text, ' @ ', (VPt[0], VPb[1]+10))
-                #font = cv2.FONT_HERSHEY_SIMPLEX
-                #fontScale=1
-                #fontThinckness=1
-                #cv2.putText(cv_img, text, (VPt[0], VPb[1]+20), font, 1, (0, 0, 200), 2, 255)
-                #cv2.putText(gImgWC, text, (10, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 200), 2, 255)
+    def show(self, cv_img, text, X=0, Y=0):
+        """To show text on given image
+        """
+        w, h, pads = self.get_textSize(text)
+        x = X + pads
+        y = Y + h + pads*2
+        cv2.putText( cv_img, text, (x, y),
+                        self.fontFace, self.fontScale,
+                        self.fontColor, self.fontThickness, 255)
 
 #---------------------------------------------------------------
 # Module Test
 #---------------------------------------------------------------
 
 def main():
-    print('I am in!')
-    dprint = DebugPrint('main')
-    print('Setting debug level: DEBUG_WARNING')
-    dprint.set_level(DEBUG_WARNING)
+    cv_img = cv2.imread("./church.jpg")
+    cv_win = "Test Image"
+    cv2.namedWindow(cv_win, cv2.WINDOW_AUTOSIZE)
 
-    dprint.info('You won\'t see this!!')
-    dprint.error('a error')
-    dprint.warning('a warning')
+    osd = osdText() #-- use default font setting
+    text = "Esc to exit !!"
+
+    osd.set_property(fontScale=1, fontThickness=1, fontColor=(0, 0, 255))
+    osd.show(cv_img, text, 10, 10)
+
+    cv2.imshow(cv_win, cv_img)
+    cv2.waitKey(0)
 
 
 
