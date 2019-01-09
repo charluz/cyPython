67400c38b8bb7550ac945192d1884f29c8878936
charlesCY <wuchangyu@gmail.com>
2019-01-09 09:31:43 +0800

    add new learning for argparse

---
+++ py_learning/my_argparse.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import argparse
+
+
+def hello(args):
+    print('Hello, {0}!'.format(args.name))
+
+
+def goodbye(args):
+    print('Goodbye, {0}!'.format(args.name))
+
+
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers()
+
+hello_parser = subparsers.add_parser('hello')
+hello_parser.add_argument('name')  # add the name argument
+hello_parser.set_defaults(func=hello)  # set the default function to hello
+
+goodbye_parser = subparsers.add_parser('goodbye')
+goodbye_parser.add_argument('name')
+goodbye_parser.set_defaults(func=goodbye)
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    args.func(args)  # call the default functio
