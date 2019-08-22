cb3183fd5218ec6b6d20dba9e163856163a1f9cb
charlesCY <wuchangyu@gmail.com>
2019-08-22 10:29:26 +0800

    Move cyModules as a standalone project

--- cyModules/__init__.py
+++
@@ -1,1 +0,0 @@
-# from .time_stamp import TimeStamp
--- cyModules/cy_COMMON.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-#-- coding: utf-8 --
-
-# import os, sys, platform
-
-
-SelectAB = lambda a, b, condt: a if condt else b
-
